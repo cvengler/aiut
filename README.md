be5dfdf44c280efa024746d85d34552cce0fc19e
Emil Engler <me@emilengler.com>
2023-12-29 19:38:58 +0100

    First working prototype

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 A web application to quickly check whether a connection is using Tor or
 not.
+
+## Usage
+
+Grab a current consensus and pass it through stdin, providing a port for
+the web server to listen on: `cat consensus.z | ./aiut :8080`
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"regexp"
+)
+
+var ips *map[string]bool
+
+func parseConsensus(input string) (*map[string]bool, error) {
+	ips := make(map[string]bool)
+
+	re := regexp.MustCompile("\nr ([A-Za-z0-9]+) ([A-Za-z0-9/+]+) ([A-Za-z0-9/+]+) ([\\d-]+) ([\\d:]+) ([\\d.]+) (\\d+) 0\n")
+	matches := re.FindAllStringSubmatch(input, -1)
+	for _, match := range matches {
+		ip4 := match[6]
+		ips[ip4] = true
+	}
+
+	re = regexp.MustCompile("\na \\[([0-9A-Za-z:]+)\\]:\\d+\n")
+	matches = re.FindAllStringSubmatch(input, -1)
+	for _, match := range matches {
+		ip6 := match[1]
+		ips[ip6] = true
+	}
+
+	return &ips, nil
+}
+
+func root(w http.ResponseWriter, req *http.Request) {
+	host, _, err := net.SplitHostPort(req.RemoteAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, is_tor := (*ips)[host]
+	if is_tor {
+		io.WriteString(w, "yes "+host)
+	} else {
+		io.WriteString(w, "no "+host)
+	}
+}
+
+func main() {
+	addr := os.Args[1]
+
+	// Read the consensus from stdin
+	reader := bufio.NewReader(os.Stdin)
+	consensus, err := io.ReadAll(reader)
+	if err != nil {
+		log.Panic(err)
+	}
+	ips, err = parseConsensus(string(consensus))
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Printf("Indexed %d unique IP addresses\n", len(*ips))
+
+	log.Println("Listening on " + addr)
+	http.HandleFunc("/", root)
+	err = http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Panic(err)
+	}
+}
